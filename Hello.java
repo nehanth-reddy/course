b8c30bd8180a0ed77edc84be875db133e27e19da
usdeals <you@example.com>
2023-12-17 19:51:29 -0500

    Hello

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello 
+{
+   public static void main(String[] args)
+   {
+        int x = 9;
+
+        if(x>10 && x<=20)
+           System.out.println("Hello");
+
+        else
+            System.out.println("bye");
+   }
+}
