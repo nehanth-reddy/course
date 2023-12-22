6f3166a3e2d6fd13c5673e47f91e5f15278501d9
usdeals <you@example.com>
2023-12-22 11:35:33 -0500

    update

--- Hello.java
+++ Hello.java
@@ -2,12 +2,12 @@ public class Hello
 {
    public static void main(String[] args)
    {
-        int x = 10;
+        int x = 11;
 
         if(x>10 && x<=20)
            System.out.println("Hello");
 
         else
-            System.out.println("bye");
+            System.out.println("Bye");
    }
 }
