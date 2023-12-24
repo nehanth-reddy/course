ec91004340ca681ac5355089486c49ddcc237f6e
usdeals <you@example.com>
2023-12-23 19:01:45 -0500

    output

--- Hello.java
+++ Hello.java
@@ -11,16 +11,4 @@ public class Hello
          System.out.println("Bye");
    }
 }
-public class Hello 
-{
-   public static void main(String[] args)
-   {
-      int x = 9;
 
-      if (x>10 && x<=20)
-         System.out.println("Hello");
-
-      else
-         System.out.println("Bye");
-   }
-}
