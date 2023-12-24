2e687c1b13aa94cc892878b5e856574ed403afa2
usdeals <you@example.com>
2023-12-23 19:01:22 -0500

    j

--- Hello.java
+++ Hello.java
@@ -2,12 +2,25 @@ public class Hello
 {
    public static void main(String[] args)
    {
-        int x = 11;
+      int x = 9;
 
-        if(x>10 && x<=20)
-           System.out.println("Hello");
+      if (x>10 && x<=20)
+         System.out.println("Hello");
 
-        else
-            System.out.println("Bye");
+      else
+         System.out.println("Bye");
+   }
+}
+public class Hello 
+{
+   public static void main(String[] args)
+   {
+      int x = 9;
+
+      if (x>10 && x<=20)
+         System.out.println("Hello");
+
+      else
+         System.out.println("Bye");
    }
 }
