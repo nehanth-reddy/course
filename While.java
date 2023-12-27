c121939de9b18c958a6a6a8255f554bb5ea10233
usdeals <you@example.com>
2023-12-27 14:33:59 -0500

    for loop

--- While.java
+++ While.java
@@ -1,17 +1,24 @@
 
+
 public class While
 {
     public static void main(String args[])
     {
-        int i = 5;
-
-        while(i<=4)
+       
+        
+        for(int i=1;i<=5;i++)
         {   
-            System.out.println("Hi " + i);
-
-            i++;
+            System.out.println("DAY " + i);
+            
+           // for(int j=1; j<=9;j++)
+           // {
+            //    System.out.println("  " + (j+8) + " - " + (j+9));
+            //}
         }
+
+
+
+
     } 
-    
-    
 }
+
