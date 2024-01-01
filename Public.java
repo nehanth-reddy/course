4306659afb7c61cfc4b3a9e501f3755cdb8dd284
usdeals <you@example.com>
2024-01-01 16:33:32 -0500

    created a new file namedPublic consists of code for method overloading

--- Public.java
+++ Public.java
@@ -1,5 +1,28 @@
-public class public 
-{
 
+class Calculator
+{
+    public int add(int n1, int n2, int n3)
+    {
+        return n1 + n2 + n3;
+    }
+    public int add(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+    public Double add(double n1, int n2)
+    {
+        return n1 + n2;
+    }
+}
+
+public class Public 
+{
+    public static void main(String args[])
+    {
+        Calculator obj = new Calculator();
+        int r1 = obj.add(3,4);
+        System.out.println(r1);
+    }
     
 }
+// method overloading
