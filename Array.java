de896d234ebd55aa7558cce596b9a52776aa2499
usdeals <you@example.com>
2024-01-02 20:26:16 -0500

    created new files named Arrays

---
+++ Array.java
@@ -0,0 +1,14 @@
+
+
+public class Array 
+{
+    public static void main(String[] args) 
+    {
+        int nums[] = {3,7,2,4};
+        nums[1] = 6;
+        System.out.println(nums[1]);
+
+    }    
+}
+
+
