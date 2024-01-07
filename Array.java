3c9fcbda436b744cfd33191cf8a6e5353440c7cd
usdeals <you@example.com>
2024-01-07 17:18:42 -0500

    created new file named multidimensional Array

--- Array.java
+++ Array.java
@@ -4,11 +4,20 @@ public class Array
 {
     public static void main(String[] args) 
     {
-        int nums[] = {3,7,2,4};
-        nums[1] = 6;
-        System.out.println(nums[1]);
-
+        int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+        
+        
+        for(int i=0;i<4;i++)
+        {
+            System.out.println(nums[i]);
+        }
+    
     }    
+
 }
 
 
